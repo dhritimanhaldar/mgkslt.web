0dd542f7f1c8e2993cf7036467d686098702cacf
Sankalp Kulshrestha <sakulshr@microsoft.com>
2018-05-19 14:40:33 +0530

    login

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppNetworkService } from '../services/app-network.service';
+import { User } from '../models/User'
+
 
 @Component({
   selector: 'app-login',
@@ -9,39 +11,18 @@ import { AppNetworkService } from '../services/app-network.service';
 export class LoginComponent implements OnInit {
 
   private appNetworkService: AppNetworkService;
+  public user: User = new User();
   
   constructor(private apns: AppNetworkService) { 
   	this.appNetworkService = apns;
+  }
 
+  login() {
+    
   }
   
   ngOnInit() {
-      // this.loading = true;
      
-      // this.appNetworkService.getUserRole().then(d=>{
-      // 	this.loading = false;
-      // 	this.roleList = d;
-      // 	if(!this.roleList.hasOwnProperty("ADMIN") && !this.roleList.hasOwnProperty("TEACHER") && !this.roleList.hasOwnProperty("PARENT")) {
-      // 		this.showEmptyAdminView = true;
-      // 	} else {
-      //     if(this.roleList.hasOwnProperty("ADMIN") ){
-      //       var adminSchoolData = this.roleList["ADMIN"]
-      //       for(var roleData of adminSchoolData) {
-      //         roleData.school.visitUrl = roleData.school.stage < 5 ? '/role/school/' + roleData.school.id : '/portal/' + roleData.school.id;
-      //       }
-      //     } else if(this.roleList.hasOwnProperty("TEACHER") ){
-      //       var teacherSchoolData = this.roleList["TEACHER"]
-      //       for(var roleData of teacherSchoolData) {
-      //         roleData.school.visitUrl = roleData.school.stage < 5 ? '' : '/portal/' + roleData.school.id;
-      //       }
-      //     } else if(this.roleList.hasOwnProperty("PARENT") ){
-      //       var parentSchoolData = this.roleList["PARENT"]
-      //       for(var roleData of parentSchoolData) {
-      //         roleData.school.visitUrl = roleData.school.stage < 5 ? '' : '/portal/' + roleData.school.id;
-      //       }
-      //     }
-      //   }
-      // });
   }
 
 }
---
+++ web/src/app/models/User.ts
@@ -0,0 +1,8 @@
+export class User {
+	id: Number;
+	name: string;
+	email: string;
+	phone: string;
+	password: string;
+	gender: string;
+}
