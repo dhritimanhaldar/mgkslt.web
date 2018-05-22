2b0e9ac60d3a6f63452c768f5249fde580b80e8e
Sankalp Kulshrestha <sakulshr@microsoft.com>
2018-05-22 09:44:35 +0530

    removed extra routes

--- web/src/app/layouts/admin-layout/admin-layout.module.ts
+++ web/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -9,12 +9,6 @@ import { LoginComponent } from '../../login/login.component';
 import { RoleComponent } from '../../role/role.component';
 import { SchoolCreationWizardComponent } from '../../school-creation-wizard/school-creation-wizard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
 import {
   MatButtonModule,
@@ -39,12 +33,6 @@ import {
     RoleComponent,
     SchoolCreationWizardComponent,
     UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
   ]
 })
 
